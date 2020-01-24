e01b58efaeda64a8a109dd52299dbb11c9a49dce
ylleonv <ylleonv@unal.edu.co>
2020-01-24 15:44:11 +0100

    Update fisher.cpp

--- src/fisher.cpp
+++ src/fisher.cpp
@@ -3,8 +3,7 @@
 #include "fisher.h"
 using namespace std;
 using namespace Rcpp ;
-#include <eigen3/Eigen/Dense>
-#include <eigen3/Eigen/Core>
+
 #include <RcppArmadillo.h>
 #include <RcppEigen.h>
 
